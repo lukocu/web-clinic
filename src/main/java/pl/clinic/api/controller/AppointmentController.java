1331638495d1d969964c28eee8ca18bcc998372d
lukoc <lukasz123237@gmail.com>
2023-09-06 17:55:52 +0200

    Add appointment.html

--- src/main/java/pl/clinic/api/controller/AppointmentController.java
+++ src/main/java/pl/clinic/api/controller/AppointmentController.java
@@ -1,37 +1,41 @@
 package pl.clinic.api.controller;
 
 import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import pl.clinic.api.dto.OfficeDoctorAvailabilityDTO;
 import pl.clinic.api.dto.mapper.OfficeDoctorAvailabilityMapper;
-import pl.clinic.business.AppointmentsService;
 import pl.clinic.business.OfficeDoctorAvailabilityService;
-import pl.clinic.business.OfficeService;
+
+import java.util.List;
 
 @Controller
 @AllArgsConstructor
 public class AppointmentController {
+
     public static final String APPOINTMENT = "/appointment";
+    public static final String APPOINTMENT_LIST = "/appointment/{officeId}";
 
-    private AppointmentsService appointmentsService;
-    private OfficeService officeService;
-   private OfficeDoctorAvailabilityService officeDoctorAvailabilityService;
-   private OfficeDoctorAvailabilityMapper officeDoctorAvailabilityMapper;
-    @GetMapping(APPOINTMENT+"/check/{officeNumber}")
-    public String listOfAvailablePage(@PathVariable("officeNumber") Integer officeNumber, Model model){
-
-        var officeAvailable = officeDoctorAvailabilityService.getAvailableHoursForOffice(officeNumber).stream()
-                .map(officeDoctorAvailabilityMapper::mapToDtoWithOfficeId)
+    private OfficeDoctorAvailabilityService officeDoctorAvailabilityService;
+    private OfficeDoctorAvailabilityMapper officeDoctorAvailabilityMapper;
+    @GetMapping(value = APPOINTMENT_LIST)
+    public String showAvailableList(@RequestParam("officeId") Integer officeId, Model model){
+        List<OfficeDoctorAvailabilityDTO> availabilityDTOs = officeDoctorAvailabilityService.getAvailableHoursForOffice(officeId).stream()
+                .map(a -> officeDoctorAvailabilityMapper.mapToDtoWithOfficeId(a))
                 .toList();
 
-        model.addAttribute("officeAvailableDTO",officeAvailable);
-
-        model.addAttribute("showAvailabilityTable", true);
+        model.addAttribute("availableList",availabilityDTOs);
 
         return "appointment";
     }
+
+
+    @GetMapping(value = APPOINTMENT)
+    public String showVisitSite() {
+        return "appointment";
+    }
+
 }
-//TODO
